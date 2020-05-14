dc9fbcb09d5bce0c50b53dc697c73f4ca5879392
Basantakhadka <rayriderkhadka5@gmail.com>
2020-05-14 21:02:11 +0545

    meeting planner added

---
+++ meeting_planner/website/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from datetime  import datetime
+from meetings.models import Meeting
+# Create your views here.
+def welcome(request):
+    return render(request,"website/welcome.html",
+                  {"meetings": Meeting.objects.all()})
+
+
+def date(request):
+    return HttpResponse('This page is serverd at' +str(datetime.now()))
+
+def about(request):
+    return HttpResponse('this is another page')
+
